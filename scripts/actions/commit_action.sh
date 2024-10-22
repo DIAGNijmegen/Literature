cfd8fb4d58c7766840cc93f56aedab63ca5625ea
Marina D'Amato <43416377+marinadamato@users.noreply.github.com>
2024-10-22 11:57:38 +0200

    add cara.bib to commit_action.sh

--- scripts/actions/commit_action.sh
+++ scripts/actions/commit_action.sh
@@ -2,7 +2,7 @@
 
 if [[ $(git diff) ]]; then
   git config --local user.name "action-bot"
-  git add diag.bib diagnoweb.bib fullstrings.bib medlinestrings.bib
+  git add diag.bib cara.bib diagnoweb.bib fullstrings.bib medlinestrings.bib
   git commit -m "Clean up by action bot"
   echo "Committed changes"
 else
